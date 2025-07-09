4c487e64c01a90fb79c0430d07048c8c4312fa89
gaddamranadheerreddy <gaddamranadheerreddy44@gmail.com>
2025-07-10 05:23:01 +0530

    added some code to bg of footer for both dark/white modes.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,11 +6,11 @@ const Footer = () => {
     const { darkMode } = useContext(ThemeContext);
 
   return (
-    <footer className={`py-5 ${darkMode ? 'bg-dark text-light' : 'bg-white text-dark'}`}>
-        {/* bg-dark text-white py-4" */}
+    <footer className={`py-5 ${darkMode ? 'bg-dark text-light' : 'bg-secondary text-dark'}`}>
+        {/* bg-dark text-white py-4" {`py-5 ${darkMode ? 'bg-dark text-light' : 'bg-white text-dark'}`} */}
       <Container className="text-center">
         <p className="mb-1">&copy; {new Date().getFullYear()} SniperThink. All rights reserved.</p>
-        <a href="#home" className="text-success text-decoration-none">
+        <a href="#home" className="text-success text-decoration-none text-light">
           Back to top ↑
         </a>
       </Container>
